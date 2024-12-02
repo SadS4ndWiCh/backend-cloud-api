ad31d15f0c1ac4644fafa38ecfc9f9e54064c49c
SadS4ndWiCh <caiocamargo007@gmail.com>
2024-12-02 07:44:20 -0300

    refator: create own module to prisma service

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 
+import { PrismaModule } from "src/providers/prisma/prisma.module";
+
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 
-import { PrismaService } from "src/providers/prisma.service";
-
 @Module({
+    imports: [PrismaModule],
     controllers: [UsersController],
-    providers: [UsersService, PrismaService]
+    providers: [UsersService]
 })
 export class UsersModule {}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 
-import { PrismaService } from "src/providers/prisma.service";
+import { PrismaService } from "src/providers/prisma/prisma.service";
 
 @Injectable()
 export class UsersService {
