9fe65c685b4350c58ec80ad02255bd3bd703aac6
SadS4ndWiCh <caiocamargo007@gmail.com>
2024-12-14 08:53:13 -0300

    feat: import files module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { UsersModule } from '../users/users.module';
 import { SessionsModule } from '../sessions/sessions.module';
+import { FilesModule } from '../files/files.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, SessionsModule],
+  imports: [UsersModule, AuthModule, SessionsModule, FilesModule],
   controllers: [],
   providers: [],
 })
